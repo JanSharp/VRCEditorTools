088553e348a4c41bba635c96bf1e251d30295cc5
JanSharp <steinmeister36@gmail.com>
2024-04-25 00:07:16 +0200

    Add tool to select particles based on culling mode

---
+++ Editor/SelectParticleSystems.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace JanSharp
+{
+    public static class SelectParticleSystems
+    {
+        [MenuItem("Tools/Select Particle Systems/Automatic")]
+        public static void SelectAutomatic() => SelectMode(ParticleSystemCullingMode.Automatic);
+        [MenuItem("Tools/Select Particle Systems/Pause And Catch-up")]
+        public static void SelectPauseAndCatchup() => SelectMode(ParticleSystemCullingMode.PauseAndCatchup);
+        [MenuItem("Tools/Select Particle Systems/Pause")]
+        public static void SelectPause() => SelectMode(ParticleSystemCullingMode.Pause);
+        [MenuItem("Tools/Select Particle Systems/Always Simulate")]
+        public static void SelectAlwaysSimulate() => SelectMode(ParticleSystemCullingMode.AlwaysSimulate);
+
+        private static void SelectMode(ParticleSystemCullingMode mode)
+        {
+            Selection.objects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects()
+                .SelectMany(go => go.GetComponentsInChildren<ParticleSystem>())
+                .Where(ps => ps.main.cullingMode == mode)
+                .Select(ps => ps.gameObject)
+                .Distinct()
+                .ToArray();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -17,3 +17,4 @@ Head to my [VCC Listing](https://jansharp.github.io/vrc/vcclisting.xhtml) and fo
   - Yes, Selectable is a base class derived by several other components
 - OcclusionVisibilityWindow to help visualize which objects are occluders and occludees (and static batchers)
 - BulkReplaceWindow to quickly replace many objects with another prefab
+- Select Particle Systems based on their culling mode
