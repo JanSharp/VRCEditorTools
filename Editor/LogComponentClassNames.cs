da6428d7d0a7733c484e4d250e922ed5287b2b0a
JanSharp <steinmeister36@gmail.com>
2024-05-20 19:50:35 +0200

    Add LogComponentClassNames menu item

---
+++ Editor/LogComponentClassNames.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Linq;
+
+namespace JanSharp
+{
+    public static class LogComponentClassNames
+    {
+        [MenuItem("Tools/JanSharp/Log Component Class Names", isValidateFunction: true, priority: 1000)]
+        public static bool ValidateDoLogComponentClassNames()
+        {
+            return Selection.activeGameObject != null;
+        }
+
+        [MenuItem("Tools/JanSharp/Log Component Class Names", isValidateFunction: false, priority: 1000)]
+        public static void DoLogComponentClassNames()
+        {
+            string components = string.Join(", ", Selection.activeGameObject.GetComponents<Component>().Select(c => c.GetType().Name));
+            Debug.Log("Component Class Names: " + components, Selection.activeGameObject);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -21,3 +21,4 @@ Head to my [VCC Listing](https://jansharp.github.io/vrc/vcclisting.xhtml) and fo
 - Save Pending Asset Changes is a tiny editor script to write any asset file changes unity has cached in memory to the drive
 - Create Parent menu item next to Create Empty, creating an empty game object which becomes the parent of all selected objects
 - Selection Stage editor window to effectively have a second much more manually controlled list of selected objects
+- Log Component Class names to quickly get the component names to be able to search for all objects with that component in the scene
