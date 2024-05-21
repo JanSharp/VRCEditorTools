17ec4f63e529a637462e50704384612c801638a0
JanSharp <steinmeister36@gmail.com>
2024-05-21 14:32:56 +0200

    Rename LogComponentClassNames to Print

--- Editor/PrintComponentClassNames.cs
+++ Editor/PrintComponentClassNames.cs
@@ -7,16 +7,16 @@ using System.Linq;
 
 namespace JanSharp
 {
-    public static class LogComponentClassNames
+    public static class PrintComponentClassNames
     {
-        [MenuItem("Tools/JanSharp/Log Component Class Names", isValidateFunction: true, priority: 1000)]
-        public static bool ValidateDoLogComponentClassNames()
+        [MenuItem("Tools/JanSharp/Print Component Class Names", isValidateFunction: true, priority: 1000)]
+        public static bool ValidateDoPrintComponentClassNames()
         {
             return Selection.activeGameObject != null;
         }
 
-        [MenuItem("Tools/JanSharp/Log Component Class Names", isValidateFunction: false, priority: 1000)]
-        public static void DoLogComponentClassNames()
+        [MenuItem("Tools/JanSharp/Print Component Class Names", isValidateFunction: false, priority: 1000)]
+        public static void DoPrintComponentClassNames()
         {
             string components = string.Join(", ", Selection.activeGameObject.GetComponents<Component>()
                 .Select(c => c == null ? "<missing-script>" : c.GetType().Name));
