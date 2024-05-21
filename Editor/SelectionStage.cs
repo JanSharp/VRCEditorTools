a73b30f679b05cbff222621a57f789847c585984
JanSharp <steinmeister36@gmail.com>
2024-05-21 09:56:54 +0200

    Significantly improve selection stage
    Add all variants of set operations. Named a bit differently, but this
      should be more intuitive. Though Flip Exclude is really confusing to
      think about, which makes it a super user feature only, effectively.
      Basically where Exclude is a left outer join, Flip Exclude is a right
      outer join, see Affecting Stage
    Consider the entire stage when nothing is selected within the stage
    Add a deselect button for clearing selection within the stage
    Automatically deselect a double clicked element if it was the only one
      selected to prevent accidental stage narrowing
    Arrange buttons into 3 columns
    Add separation between the list and the buttons by using 2 boxes
    Add some TODOs

--- Editor/SelectionStage.cs
+++ Editor/SelectionStage.cs
@@ -1,10 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
 using System.Linq;
-using System.Text.RegularExpressions;
+
+// TODO: Undo support for changes within the selection stage!
+// TODO: Figure out how tooltips work.
+// TODO: Once upgrading to 2022 add more and better selection support within the stage.
 
 namespace JanSharp
 {
@@ -14,9 +16,11 @@ namespace JanSharp
         private HashSet<GameObject> stagedLut;
         private ListView listView;
         private List<object> listViewSelected;
-        private IEnumerable<GameObject> SelectedGameObjects => listViewSelected.Where(obj => obj != null).Cast<GameObject>();
+        private IEnumerable<GameObject> SelectedInStage => listViewSelected.Any(obj => obj != null)
+            ? listViewSelected.Where(obj => obj != null).Cast<GameObject>()
+            : staged;
 
-        [MenuItem("Tools/JanSharp/Selection Stage", false, 980)]
+        [MenuItem("Tools/JanSharp/Selection Stage", priority = 980)]
         public static void ShowSelectionStage()
         {
             // This method is called when the user selects the menu item in the Editor
@@ -27,55 +31,104 @@ namespace JanSharp
 
         private void CreateGUI()
         {
-            VisualElement root = rootVisualElement;
-
+            // TODO: don't reset here because every recompile causes the stage to get cleared because of this.
             staged = new List<GameObject>();
             stagedLut = new HashSet<GameObject>();
             listViewSelected = new List<object>(); // Just to make it never be null.
 
-            System.Func<VisualElement> makeItem = () => {
-                return new Label();
-            };
+            VisualElement root = rootVisualElement;
+
+            Box listBox = new Box() { style = { flexGrow = 1f } };
+
+            System.Func<VisualElement> makeItem = () => new Label();
             System.Action<VisualElement, int> bindItem = (element, index) => {
                 Label label = (Label)element;
                 label.text = staged[index].name;
             };
-            listView = new ListView(staged, 16, makeItem, bindItem);
-            listView.style.flexGrow = 1.0f;
-            listView.selectionType = SelectionType.Multiple;
-
+            listView = new ListView(staged, 16, makeItem, bindItem)
+            {
+                style = { flexGrow = 1f },
+                selectionType = SelectionType.Multiple,
+            };
             listView.onItemChosen += obj => {
                 GameObject go = (GameObject)obj;
                 EditorGUIUtility.PingObject(go); // Jump to in hierarchy, without selecting.
-
                 var prev = Selection.objects;
                 Selection.activeObject = go;
                 SceneView.FrameLastActiveSceneView();
                 Selection.objects = prev;
+                if (listViewSelected.Count == 1 && ((GameObject)listViewSelected[0]) == go)
+                {
+                    // When double clicking a single element, clear the selection again as to prevent
+                    // accidentally narrowing the stage through having only 1 element selected.
+                    listView.selectedIndex = -1;
+                    listView.Refresh();
+                }
             };
-
             listView.onSelectionChanged += selected => listViewSelected = selected;
+            listBox.Add(listView);
 
-            root.Add(listView);
+            root.Add(listBox);
 
-            root.Add(new Button(AddToStage) { text = "Add to Stage" });
-            root.Add(new Button(ExcludeFromStage) { text = "Exclude from Stage" });
-            root.Add(new Button(RemoveFromStage) { text = "Remove from Stage" });
-            root.Add(new Button(ClearStage) { text = "Clear Stage" });
-            root.Add(new Button(ApplySelected) { text = "Apply selected" });
-            root.Add(new Button(OverwriteSelected) { text = "Overwrite selected" });
+            Box buttonsBox = new Box();
+            IMGUIContainer buttonColumns = new IMGUIContainer() { style = { flexDirection = FlexDirection.Row } };
+
+            {
+                IMGUIContainer column = new IMGUIContainer() { style = { flexGrow = 1f } };
+                column.Add(new Label("Affect Stage")
+                {
+                    style = { alignSelf = Align.Center },
+                    tooltip = "How should the current selection affect the stage?",
+                });
+                column.Add(new Button(AddToStage) { text = "Add" });
+                column.Add(new Button(ExcludeFromStage) { text = "Exclude" });
+                column.Add(new Button(FlipExcludeFromStage) { text = "Flip Exclude" });
+                column.Add(new Button(IntersectWithStage) { text = "Intersect" });
+                column.Add(new Button(SymmetricDifferenceWithStage) { text = "Symmetric Diff" });
+                column.Add(new Button(OverwriteStage) { text = "Overwrite" });
+                buttonColumns.Add(column);
+            }
+
+            {
+                IMGUIContainer column = new IMGUIContainer() { style = { flexGrow = 1f } };
+                column.Add(new Label("Affect Selection")
+                {
+                    style = { alignSelf = Align.Center },
+                    tooltip = "How should the current stage affect the Selection?\n"
+                        + "When nothing is selected within the stage, the entire stage is considered.",
+                });
+                column.Add(new Button(AddToSelection) { text = "Add", });
+                column.Add(new Button(ExcludeFromSelection) { text = "Exclude" });
+                column.Add(new Button(FlipExcludeFromSelection) { text = "Flip Exclude" });
+                column.Add(new Button(IntersectWithSelection) { text = "Intersect" });
+                column.Add(new Button(SymmetricDifferenceWithSelection) { text = "Symmetric Diff" });
+                column.Add(new Button(OverwriteSelection) { text = "Overwrite" });
+                buttonColumns.Add(column);
+            }
+
+            {
+                IMGUIContainer column = new IMGUIContainer() { style = { flexGrow = 1f } };
+                column.Add(new Label("Within Stage") { style = { alignSelf = Align.Center } });
+                column.Add(new Button(DeselectWithinStage) { text = "Deselect" });
+                column.Add(new Button(RemoveWithinStage) { text = "Remove" });
+                column.Add(new Button(ClearStage) { text = "Clear" });
+                buttonColumns.Add(column);
+            }
+
+            buttonsBox.Add(buttonColumns);
+            root.Add(buttonsBox);
         }
 
-        private void RemoveObjectsFromStage(HashSet<GameObject> toRemove)
+        private void RemoveObjectsFromStage(IEnumerable<GameObject> toRemove, bool inverted = false)
         {
-            if (toRemove.Count == 0)
+            if (!inverted && !toRemove.Any())
                 return;
             int c = staged.Count;
             int newI = 0;
             for (int i = 0; i < c; i++)
             {
                 GameObject go = staged[i];
-                if (toRemove.Contains(go))
+                if (toRemove.Contains(go) != inverted)
                     stagedLut.Remove(go);
                 else
                     staged[newI++] = go;
@@ -85,15 +138,25 @@ namespace JanSharp
             listView.Refresh();
         }
 
+        private void OverwriteStageEntirely(ICollection<GameObject> newSelection)
+        {
+            staged.Clear();
+            staged.AddRange(newSelection);
+            stagedLut = new HashSet<GameObject>(staged);
+            listView.selectedIndex = -1;
+            listView.Refresh();
+        }
+
         private void Cleanup()
         {
-            RemoveObjectsFromStage(new HashSet<GameObject>(staged.Where(go => go == null)));
+            RemoveObjectsFromStage(staged.Where(go => go == null));
         }
 
         private void OnHierarchyChange()
         {
             Cleanup();
         }
+
 
         private void AddToStage()
         {
@@ -109,12 +172,78 @@ namespace JanSharp
 
         private void ExcludeFromStage()
         {
-            RemoveObjectsFromStage(new HashSet<GameObject>(Selection.gameObjects));
+            RemoveObjectsFromStage(Selection.gameObjects);
         }
 
-        private void RemoveFromStage()
+        private void FlipExcludeFromStage()
         {
-            RemoveObjectsFromStage(new HashSet<GameObject>(SelectedGameObjects));
+            OverwriteStageEntirely(Selection.gameObjects.Except(SelectedInStage).ToList());
+        }
+
+        private void IntersectWithStage()
+        {
+            RemoveObjectsFromStage(Selection.gameObjects, inverted: true);
+        }
+
+        private void SymmetricDifferenceWithStage()
+        {
+            GameObject[] selected = Selection.gameObjects;
+            OverwriteStageEntirely(SelectedInStage
+                .Union(selected)
+                .Except(SelectedInStage.Intersect(selected))
+                .ToList());
+        }
+
+        private void OverwriteStage()
+        {
+            OverwriteStageEntirely(Selection.gameObjects);
+        }
+
+
+        private void AddToSelection()
+        {
+            Selection.objects = Selection.gameObjects.Union(SelectedInStage).ToArray();
+        }
+
+        private void ExcludeFromSelection()
+        {
+            Selection.objects = Selection.gameObjects.Except(SelectedInStage).ToArray();
+        }
+
+        private void FlipExcludeFromSelection()
+        {
+            Selection.objects = SelectedInStage.Except(Selection.gameObjects).ToArray();
+        }
+
+        private void IntersectWithSelection()
+        {
+            Selection.objects = Selection.gameObjects.Intersect(SelectedInStage).ToArray();
+        }
+
+        private void SymmetricDifferenceWithSelection()
+        {
+            GameObject[] selected = Selection.gameObjects;
+            Selection.objects = selected
+                .Union(SelectedInStage)
+                .Except(selected.Intersect(SelectedInStage))
+                .ToArray();
+        }
+
+        private void OverwriteSelection()
+        {
+            Selection.objects = SelectedInStage.ToArray();
+        }
+
+
+        private void DeselectWithinStage()
+        {
+            listView.selectedIndex = -1;
+        }
+
+        private void RemoveWithinStage()
+        {
+            // Not using SelectedInStage since this should not clear the stage when nothing is selected.
+            RemoveObjectsFromStage(listViewSelected.Where(obj => obj != null).Cast<GameObject>());
         }
 
         private void ClearStage()
@@ -122,16 +251,6 @@ namespace JanSharp
             staged.Clear();
             stagedLut.Clear();
             listView.Refresh();
-        }
-
-        private void ApplySelected()
-        {
-            Selection.objects = Selection.gameObjects.Union(SelectedGameObjects).ToArray();
-        }
-
-        private void OverwriteSelected()
-        {
-            Selection.objects = SelectedGameObjects.ToArray();
         }
     }
 }
